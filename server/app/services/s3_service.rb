cc472acd987be868cb9ce5b75b4b3e39efb32dd3
Minvydas Staniulis <mimciks@gmail.com>
2022-06-08 14:43:59 +0300

    uploading file to created bucket on aws

--- server/app/services/s3_service.rb
+++ server/app/services/s3_service.rb
@@ -1,6 +1,36 @@
 require 'aws-sdk'
 
 class S3Service
+    def object_uploaded?(s3, bucket_name, object_key)
+        response = s3.put_object(
+            bucket: bucket_name,
+            key: object_key
+        )
+        if response.etag
+            return true
+        else 
+            return false 
+        end 
+    rescue StandardError => e
+        puts "Error uploading object: #{e.message}"
+        return false  
+    end
 
+    def run()
+        #creating aws client for S3 service
+        s3 = Aws::S3::Client.new(
+                access_key_id: Rails.application.credentials.aws.access_key_id,
+                secret_access_key: Rails.application.credentials.aws.secret_access_key,
+                region: Rails.application.credentials.aws.region
+            )
+        bucket_name = 'team3-pdfers-rateconfocr-bucket'
+        object_key = 'test1.pdf'
+
+        if object_uploaded?(s3, bucket_name, object_key)
+            puts "Object '#{object_key}' uploaded to bucket - '#{bucket_name}'."
+        else
+            puts "Object '#{object_key}' not uploaded to bucket - '#{bucket_name}'."
+        end
+    end
 
 end
