ae9855727ec82faf01836aae8c0355d85fcb7c8e
Minvydas Staniulis <mimciks@gmail.com>
2022-06-08 15:21:59 +0300

    uploading file to aws s3 bug fix

--- server/app/services/s3_service.rb
+++ server/app/services/s3_service.rb
@@ -2,23 +2,13 @@ require 'aws-sdk'
 
 class S3Service
     def object_uploaded?(s3, bucket_name, object_key)
-        response = s3.put_object(
-            bucket: bucket_name,
-            key: object_key
-        )
-        if response.etag
-            return true
-        else 
-            return false 
-        end 
-    rescue StandardError => e
-        puts "Error uploading object: #{e.message}"
-        return false  
+        response = s3.bucket(bucket_name).object(object_key).upload_file('app/services/test1.pdf')
+    
     end
 
     def run()
         #creating aws client for S3 service
-        s3 = Aws::S3::Client.new(
+        s3 = Aws::S3::Resource.new(
                 access_key_id: Rails.application.credentials.aws.access_key_id,
                 secret_access_key: Rails.application.credentials.aws.secret_access_key,
                 region: Rails.application.credentials.aws.region
