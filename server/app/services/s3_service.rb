9b9be66307d65fc7179ed1fdd6136cc2623a7813
Minvydas Staniulis <mimciks@gmail.com>
2022-06-08 16:29:42 +0300

    check if upload is successful

--- server/app/services/s3_service.rb
+++ server/app/services/s3_service.rb
@@ -2,8 +2,9 @@ require 'aws-sdk'
 
 class S3Service
     def object_uploaded?(s3, bucket_name, object_key)
-        response = s3.bucket(bucket_name).object(object_key).upload_file('app/services/test1.pdf')
-    
+        response = s3.bucket(bucket_name).object(object_key).upload_file('app/services/test1.pdf') #file location
+    rescue 
+        false
     end
 
     def run()
@@ -13,8 +14,8 @@ class S3Service
                 secret_access_key: Rails.application.credentials.aws.secret_access_key,
                 region: Rails.application.credentials.aws.region
             )
-        bucket_name = 'team3-pdfers-rateconfocr-bucket'
-        object_key = 'test1.pdf'
+        bucket_name = 'team3-pdfers-rateconfocr-bucket' #always remains the same
+        object_key = 'test1.pdf' #how to name the file
 
         if object_uploaded?(s3, bucket_name, object_key)
             puts "Object '#{object_key}' uploaded to bucket - '#{bucket_name}'."
