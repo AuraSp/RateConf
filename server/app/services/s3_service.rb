fd83fc3804803bcb0c43809818dd07a40db759b4
Minvydas Staniulis <mimciks@gmail.com>
2022-06-09 10:17:12 +0300

    small update

--- server/app/services/s3_service.rb
+++ server/app/services/s3_service.rb
@@ -1,8 +1,8 @@
 require 'aws-sdk'
 
 class S3Service
-    def object_uploaded?(s3, bucket_name, object_key)
-        response = s3.bucket(bucket_name).object(object_key).upload_file('app/services/test1.pdf') #file location
+    def object_uploaded?(s3, bucket_name, object_key, path)
+        response = s3.bucket(bucket_name).object(object_key).upload_file(path) #file location
     rescue 
         false
     end
@@ -14,10 +14,13 @@ class S3Service
                 secret_access_key: Rails.application.credentials.aws.secret_access_key,
                 region: Rails.application.credentials.aws.region
             )
-        bucket_name = 'team3-pdfers-rateconfocr-bucket' #always remains the same
-        object_key = 'test1.pdf' #how to name the file
 
-        if object_uploaded?(s3, bucket_name, object_key)
+        path = 'app/services/test1.pdf'
+
+        bucket_name = 'team3-pdfers-rateconfocr-bucket' #always remains the same
+        object_key = File.basename(path) #how to name the file
+
+        if object_uploaded?(s3, bucket_name, object_key, path)
             puts "Object '#{object_key}' uploaded to bucket - '#{bucket_name}'."
         else
             puts "Object '#{object_key}' not uploaded to bucket - '#{bucket_name}'."
