2fdf7856f1bd7b94dcfa995626b2be1d20b83810
Minvydas Staniulis <mimciks@gmail.com>
2022-06-10 12:07:22 +0300

    update to be able change fields after autofill

--- frontend/src/components/Fields.js
+++ frontend/src/components/Fields.js
@@ -1,22 +1,36 @@
 import React from 'react'
 import Autofill from './Autofill'
 
-const Fields = ( props ) => {
+import { useState, useEffect } from 'react'
+
+const Fields = ( {data} ) => {
+
+  const [fieldData, setFieldData] = useState(data);
+
+  useEffect(() => {
+      setFieldData(data);
+  }, [data]);
+
+  const handleChange = e => {
+      setFieldData({
+          [e.target.name]: e.target.value
+      });
+  }
+
   return (
     <div>
-
         <form>
             <label id='name-field'>
                 Name:
-                <input id='name-input' type="text" name="name" value={props.data.name}/>
+                <input id='name-input' type="text" name="name" value={fieldData.name} onChange={handleChange}/>
             </label>
             <label id='surname-field'>
                 Surname:
-                <input id='surname-input' type="text" name="surname" value={props.data.surname}/>
+                <input id='surname-input' type="text" name="surname" value={fieldData.surname} onChange={handleChange}/>
             </label>
             <label id='country-field'>
                 Country:
-                <input id='country-input' type="text" name="country" value={props.data.country}/>
+                <input id='country-input' type="text" name="country" value={fieldData.country} onChange={handleChange}/>
             </label>
         </form>
     </div>
