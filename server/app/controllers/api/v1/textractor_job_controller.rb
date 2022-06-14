6019f4d005fd9d0573866dd2466cd12c0e86524c
Minvydas Staniulis <mimciks@gmail.com>
2022-06-14 11:17:10 +0300

    small error handling

--- server/app/controllers/api/v1/textractor_job_controller.rb
+++ server/app/controllers/api/v1/textractor_job_controller.rb
@@ -3,6 +3,11 @@ class Api::V1::TextractorJobController < ApplicationController
     def create
         company = params[:company]
         rateConfData = TextractorJobService.new.run(params[:jobID], company)
-        render json: {Extracted: rateConfData}
+
+        render json: {status: "SUCCESS", Extracted: rateConfData}, status: :ok
+    rescue Exception => ex
+        render json: {status: "FAILURE", Extracted: "Unable to receive extracted data"}, status: 500
+    ensure
+
     end
 end
