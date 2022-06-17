a3341b949500ef3ea4b8dac9ea9b58def310cb5a
Aura <aura@openroadtms.com>
2022-06-17 14:25:41 +0300

    data rendering

--- frontend/src/components/QueryCheck.js
+++ frontend/src/components/QueryCheck.js
@@ -5,6 +5,7 @@ import axios from "axios";
 const QueryCheck = () => {
   const [queryId, setQueryId] = useState('');
   const [response, setResponse] = useState([]);
+  const [data, setData] = useState();
 
   const handleChangeQuery = event => {
     setQueryId(event.target.value);
@@ -18,7 +19,7 @@ const QueryCheck = () => {
 
     console.log(data);
 
-    const url = 'http://localhost:5000/api/v1/pdf_api' + '?id=' + queryId; 
+    const url = 'http://localhost:5000/api/v1/pdf_api' + '?id=' + queryId;
 
     axios.get(url, {
       headers: {
@@ -35,28 +36,28 @@ const QueryCheck = () => {
   };
 
   const updateData = () => {
-     if(response.length !== 0) {
-    //   if(response.query[0].status === "finished") {
-    //     //console.log("finished!!!! :)))))))");
-    //     const stringData = response.query[0].rateConfData;
-    //     var requiredData = stringData.match(/"([^']+)"/)[0];
-    //     var reg = /"(.*?)"/i;
-    //     var allData = [];
-    //     //15
-    //     for(let i = 0; i < 15; i++) {
-    //       allData.push(requiredData.match(reg)[1]);
-    //       requiredData = requiredData.replace(requiredData.match(reg)[0], '')
-    //     }
-    //     //console.log(allData)
-    //     document.getElementById("jobStatus").innerHTML = "Job is finished."
-    //     document.getElementById("responseData").innerHTML = allData;
-    //   }
-    //   else if(response.query[0].status === "processing") {
-    //     console.log("The Document is still being processed");
-    //     document.getElementById("jobStatus").innerHTML = "The Document is still being processed."
-    //   }
-      document.getElementById("responseData").innerHTML = response.query[0];
-     //console.log(response.query[0].rate_conf_data)
+    if (response.length !== 0) {
+      if (response.query[0].status === "finished") {
+        //console.log("finished!!!! :)))))))");
+        const stringData = response.query[0].rate_conf_data;
+        var requiredData = stringData.match(/"([^']+)"/)[0];
+        var reg = /"(.*?)"/i;
+        var allData = [];
+        //15
+        for (let i = 0; i < 15; i++) {
+          allData.push(requiredData.match(reg)[1]);
+          requiredData = requiredData.replace(requiredData.match(reg)[0], '')
+        }
+        //console.log(allData)
+        console.log("JOb is finished");
+        document.getElementById("jobStatus").innerHTML = "Job is finished."
+        setData(response.query[0])
+      }
+      else if (response.query[0].status === "processing") {
+        console.log("The Document is still being processed");
+        document.getElementById("jobStatus").innerHTML = "The Document is still being processed."
+        setData(response.query[0])
+      }
     }
   }
 
@@ -64,20 +65,22 @@ const QueryCheck = () => {
     updateData();
   }, [response])
 
+  console.log(data)
+
   return (
     <div>
       <div id="checkQuery">
-      <h2>Check by entering your Query ID</h2>
-          <form>
-              <label>
-                  <input id="queryIdInput" value={queryId} onChange={handleChangeQuery} type="text"></input>
-              </label>
-              <button id='requestButton' onClick={handleClick}>Click me</button>
-          </form>
+        <h2>Check by entering your Query ID</h2>
+        <form>
+          <label>
+            <input id="queryIdInput" value={queryId} onChange={handleChangeQuery} type="text"></input>
+          </label>
+          <button id='requestButton' onClick={handleClick}>Click me</button>
+        </form>
+        <div>
           <h3 id='jobStatus'></h3>
-          <p id='responseData'></p>
-          {updateData}
-          
+          <p>{JSON.stringify(data.rate_conf_data)}</p>
+        </div>
       </div>
     </div>
   )
