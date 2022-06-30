1469499ae505f21dbfe19f230e709f7a6100e500
Aura <aura@openroadtms.com>
2022-06-30 14:37:49 +0300

    swapped position

--- frontend/src/components/QueryCard.js
+++ frontend/src/components/QueryCard.js
@@ -7,10 +7,10 @@ const QueryCard = ({ response }) => {
             <ul className='list' key={response.id}>
                 <li><span>ID: </span>{response.id}</li>
                 <li><span>Query_id: </span>{response.query_id}</li>
-                <li><span>Error data: </span>{response.error_data}</li>
+                <li><span>Enquirer: </span>{response.enquirer}</li>
                 <li><span>Aws s3: </span>{response.aws_s3_name}</li>
                 <li><span>Status: </span>{response.status}</li>
-                <li><span>Enquirer: </span>{response.enquirer}</li>
+                <li><span>Error data: </span>{response.error_data}</li>
                 <li><span>Created at: </span>{response.created_at}</li>
             </ul>
         </>
