4597fe78e5c5a7b9a65c1114936fcc8b33114d80
Aura <aura@openroadtms.com>
2022-06-30 15:07:49 +0300

    fixed handlers

--- server/app/services/pdf_service.rb
+++ server/app/services/pdf_service.rb
@@ -5,28 +5,25 @@ require "chilkat"
 class PdfService
   begin
     def decodePdfFromB64(b64, queryId)
-      begin
-        tempFilePath = "/tmp/#{queryId}.pdf"
-        File.open(tempFilePath, "wb") do |f|
-          f.write(Base64.decode64(b64))
-        end
-
-        return tempFilePath
+      tempFilePath = "/tmp/#{queryId}.pdf"
+      File.open(tempFilePath, "wb") do |f|
+        f.write(Base64.decode64(b64))
       rescue LoadError => ex
       end
+
+      return tempFilePath
     end
 
     def encodePdfToB64(path)
       begin
-          if (!File.file?(path))
-            print "failed to load PDF file."
-            exitF
+        if (!File.file?(path))
+          print "failed to load PDF file."
+          exitF
         end
 
         file = open(path)
         b64 = Base64.encode64(file.read)
         return b64
-
       rescue Encoding::InvalidByteSequenceError => ex
       end
     end
@@ -37,17 +34,15 @@ class PdfService
 
     # Convert converted pdf into image for future slicing based on extracted data
     def convertPdfToImage
-      begin
-        image = Magick::Image::from_blob(convertedPdf) do
-          self.format = "PDF"
-          self.quality = 100
-          self.density = 160
-        end
-        image[0].format = "JPG"
-        image[0].to_blob
-        return image[0]
+      image = Magick::Image::from_blob(convertedPdf) do
+        self.format = "PDF"
+        self.quality = 100
+        self.density = 160
       rescue Custom::ImageConvertingError => ex
       end
+      image[0].format = "JPG"
+      image[0].to_blob
+      return image[0]
     end
   rescue Custom::PdfService => ex
   end
